41b56a74270d1184c1a768c2c0ddce3378077d35
javier <javiervelaz@hotmail.com>
2021-07-06 19:43:29 -0300

    primero

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,47 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        home
+        <p ref="p">mi nombre es  {{name }} y mi edad es {{ age}}</p>
+        <button @click="handleClick">Click me</button>
+        <button @click="age++">add 1 to age</button>
+        <input type="text" v-model="name" />
+    </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
+    import { ref ,reactive} from 'vue'
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+        name: 'Home',
+        setup() {
+            console.log('setup')
+
+            
+            //const p = ref(null)
+            const name = ref('mario')
+            const age = ref(30)
+
+            const handleClick = () => {
+                name.value = 'javier'
+                age.value = 89  
+            }
+
+            return {
+                name, age, handleClick
+            }
+        },
+        data() {
+            return {
+                
+            }
+        },
+        created() {
+            console.log('created')
+        },
+        mounted() {
+            console.log('mounted')
+        },
+        components: {}
 }
 </script>
